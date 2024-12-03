7bec9f41350303347bebb0d4bed0ecbf9406263b
DaniMP84 <xDani84x@gmail.com>
2024-12-03 15:42:53 +0100

    Creación de variables y "moneda"
    He creado las variables que se me han ocurrido hasta ahora y la forma de elegir quien empieza.

---
+++ src/TresRaya/TresEnRaya.java
@@ -0,0 +1,34 @@
+package TresRaya;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TresEnRaya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);		
+		Random moneda = new Random();
+		
+		String jugador1;
+		String jugador2;
+		int [][] tablero = {{'*','*','*'},
+							{'*','*','*'},
+							{'*','*','*'}};
+		
+		//Pregunto los nombres de los jugadores y guardo la respuesta en las variables
+		System.out.println("Introduce nombre de jugador 1:");
+		jugador1 = in.nextLine();
+		System.out.println("Introduce nombre de jugador 2:");
+		jugador2 = in.nextLine();
+		
+		/*Random entre dos opciones, si sale 0 jugador1, si sale 1 es jugador2
+		es como hacer un if.*/
+		String primeroQueJuega = moneda.nextInt(2) == 0 ? jugador1 : jugador2;
+		System.out.println("Primero le toca jugar a " + primeroQueJuega);
+		
+		
+	}
+
+}
